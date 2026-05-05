796df81fc8a51ff8599c6db213dfec960772fc63
jaskaranvirk01 <virk7184@gmail.com>
2026-05-05 11:13:20 +0530

    homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+def footer_home():
+    owner_name = "Jaskaran Singh"
+
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created by </p>  
+        <p style="font-weight:bold; color:white;">{owner_name}</p>
+        </div>
+                
+                """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    owner_name = "Jaskaran Singh"
+
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with by </p>  
+        <p style="font-weight:bold; color:black;">{owner_name} </p>
+        </div>
+                
+                """, unsafe_allow_html=True)
